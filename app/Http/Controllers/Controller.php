33d6d4eae6fdf07d9bd78d976bb3a86b53cdeb5b
Arqam Zia <arqam.mzia@gmail.com>
2025-02-26 23:23:17 +0500

    Define methods in abstract class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 abstract class Controller
 {
-    //
+    /**
+     * Return a success response.
+     *
+     * @param string $message
+     * @param mixed $data
+     * @param int $statusCode
+     * @return JsonResponse
+     */
+    abstract protected function sendSuccess($message, $data = null, $statusCode = 200): JsonResponse;
+
+    /**
+     * Return an error response.
+     *
+     * @param string $message
+     * @param mixed $data
+     * @param int $statusCode
+     * @return JsonResponse
+     */
+    abstract protected function sendError($message, $data = null, $statusCode = 400): JsonResponse;
+
+    /**
+     * Handle exceptions in a standard way.
+     *
+     * @param \Exception $e
+     * @return JsonResponse
+     */
+    abstract protected function handleException(\Exception $e): JsonResponse;
 }
